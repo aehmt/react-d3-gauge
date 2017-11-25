1b770f1166d020b84210df59593bd11c5006c62d
Ahmet Calis <calisahmet@gmail.com>
2017-11-24 22:33:32 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -172,7 +172,7 @@ Gauge.defaultProps = {
   barWidth: 40,
   numSections: 3,
   colors: ['#73de2c', '#e9e61a', '#e92213'],
-  needleColor: '#464A4F'
+  needleColor: 'red'
 };
 
 Gauge.propTypes = {
