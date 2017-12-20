c5c99a11565cf141c440c913b5713b49ba972138
aehmt <calisahmet@gmail.com>
2017-12-20 12:54:13 -0500

    area ratios

--- src/index.js
+++ src/index.js
@@ -2,7 +2,29 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import * as d3 from 'd3';
 
-const drawGauge = props => {
+class Gauge extends Component {
+
+  constructor(props) { 
+    super(props)
+
+    const x = () => {
+      var text = "";
+      var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+      for (var i = 0; i < 5; i++) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+      }
+      return text;
+    }
+
+    const key = x()
+    this.state = {
+      key: key
+    }
+  }
+
+  componentDidMount() {
+
+const drawGauge = (props,key) => {
   let Needle,
     arc,
     arcEndRad,
@@ -156,15 +178,16 @@ const drawGauge = props => {
   }
 };
 
-class Gauge extends Component {
-  componentDidMount() {
-    drawGauge(this.props);
+
+
+
+    drawGauge(this.props,this.state.key);
   }
 
   render() {
     return (
       <span>
-        <div style={{width: this.props.width, display: 'inline'}} className="fc822f8a-5edc-41ca-a557-93ec4b5970b7" />
+        <div style={{width: this.props.width, display: 'inline'}} className={this.state.key} />
       </span>
     );
   }
